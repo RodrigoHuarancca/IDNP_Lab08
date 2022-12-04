b1a97179473f0ad4abf830c9971d271dd47764d4
Rodrigo <rhuarancca@unsa.edu.pe>
2022-12-03 20:29:41 -0500

    sustitutorio

---
+++ Sustitutorio/app/src/main/java/com/example/sustitutorio/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sustitutorio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity extends {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
